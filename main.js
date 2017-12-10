580f7acbedd6a8c1761c478eb7cfc448c5aada80
Toan Nguyen <canhtoannguyen60@gmail.com>
2017-12-10 17:11:30 +0700

    First Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello Vue!'
+//   }
+// })
+
+var app2 = new Vue({
+  el: '#background',
+  data: {
+      imgSrc: '2.jpg'
+  }
+})
+
+
