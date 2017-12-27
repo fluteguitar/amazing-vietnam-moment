bbc7a8c73ee895a3781a58149535c61c6af45a94
Toan Nguyen <canhtoannguyen60@gmail.com>
2017-12-27 22:53:48 +0700

    add caption

---
+++ source-code/js/app/background.js
@@ -0,0 +1,23 @@
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello Vue!'
+//   }
+// })
+
+// let genRandomImage = function() {
+//     let index = getRandomInt(0, listImage.length -1 );
+//     return imagePath + listImage[index] + '.jpg';
+// } 
+// let app2 = new Vue({
+//     el: '.background',
+//     data: {
+// 	styleObject: {
+// 	    "background-color": '#ff6600',
+// 	    "background-image": 'url(' + genRandomImage() + ')'
+// 	},
+// 	imgSrc: '2.jpg'
+//     }
+// })
+
+
